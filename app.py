78dae26f6cdc8a487c18ef8970d1f1db9850dfeb
ChethanKMurthy <147957697+ChethanKMurthy@users.noreply.github.com>
2023-11-09 19:51:08 +0530

    FINAL -GOOD UPDSYYE

--- app.py
+++ app.py
@@ -1,47 +1,29 @@
-from flask import Flask, render_template, request, jsonify, redirect
+from flask import Flask, render_template, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# Define simple responses for the AI companion
-responses = {
-    "hello": "Hello there! How can I help you today?",
-    "goodbye": "Goodbye! Have a nice day.",
-    "how are you": "I'm doing well, thank you for asking.",
-    "what can you do": "I can help you with various tasks, such as providing information, answering questions, and scheduling reminders.",
-    "name": "My name is AI Companion.",
-}
+# Replace <password> with your actual MongoDB Atlas password
+uri = "mongodb+srv://chethankeshavmurthy:okchethan123@cluster0.jvsptl5.mongodb.net/?retryWrites=true&w=majority"
 
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        old_age_home = request.form['old_age_home']
+# Create a new client and connect to the server
+client = MongoClient(uri)
 
-        # Validate login credentials and redirect to portal
-        return redirect('/portal')
+# Access the 'Cluster0' database
+db = client['Cluster0']
 
-    return render_template('index.html')
+# Access the 'menstrual_data' collection
+menstrual_data = db['menstrual_data']
 
-@app.route('/portal', methods=['GET'])
-def portal():
-    return render_template('index.html')
+@app.route('/visualize')
+def visualize():
+    # Get the data from MongoDB
+    data = menstrual_data.find()
 
-@app.route('/chat', methods=['GET', 'POST'])
-def chat():
-    if request.method == 'POST':
-        user_input = request.form['user_input']
+    # Get the dark mode preference from the query string (if set)
+    dark_mode = request.args.get('dark_mode', default='light')
 
-        # Check if the user input matches any of the predefined responses
-        response = responses.get(user_input.lower())
-
-        # If no matching response is found, provide a generic response
-        if response is None:
-            response = "I'm still learning and may not have an answer for that yet."
-
-        return jsonify({'response': response})
-
-    return render_template('index.html')
+    return render_template('visualize.html', data=data, dark_mode=dark_mode)
 
 if __name__ == '__main__':
     app.run(debug=True)
